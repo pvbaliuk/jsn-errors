b010d24558e7f37fb1fafc7b3e55b98cec0d484e
Pavlo Baliuk <pv.baliuk@gmail.com>
2026-09-25 00:13:38 +0300

    Fix

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export {createRootError, type AnyRootErrorInstance, type ErrorContext, type ErrorParams, type CreateErrorOptions} from './root-error';
+export {createRootError, type AnyRootErrorInstance, type ErrorContext, type ErrorParams} from './root-error';
 export {createErrors} from './create-errors';
